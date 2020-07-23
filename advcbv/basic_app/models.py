42b5604f0d6de4a169da0da0fae6ee1cb82efab7
Ian Chou <ianchou19@gmail.com>
2020-07-23 10:21:58 -0400

    CRUD Views

--- advcbv/basic_app/models.py
+++ advcbv/basic_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+
+# Create your models here.
 
 
 class School(models.Model):
@@ -9,12 +12,15 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("basic_app:detail", kwargs={'pk': self.pk})
+
 
 class Student(models.Model):
     name = models.CharField(max_length=256)
     age = models.PositiveIntegerField()
     school = models.ForeignKey(
-        School, related_name='students', on_delete=models.CASCADE)
+        School, related_name='students', on_delete=models.CASCADE,)
 
     def __str__(self):
         return self.name
