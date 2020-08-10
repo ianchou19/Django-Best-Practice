5fed01eb55ccf584849885b449ed671bee7ca716
Ian Chou <ianchou19@gmail.com>
2020-08-10 11:08:25 -0400

    Adavanced Topics - Debug Toolbar

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from appTwo import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     path('admin/', admin.site.urls),
     url(r'^users/', include('appTwo.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
